ba025bc17ec10489c3df70ee9f360f887ade1a65
tronexis <tronexis>
2022-10-21 12:26:21 +0600

    fix: fix timeline scrolling and pre-footer section

--- src/pages/welcome.ts
+++ src/pages/welcome.ts
@@ -1,1 +1,35 @@
-export class Welcome {}
+import locomotiveScroll from "locomotive-scroll";
+
+export default class Welcome {
+  
+
+}
+
+// export default {
+//   name: "locoScroll",
+//   props: {
+//     msg: String
+//   },
+//   data() {
+//     return {
+//       scrollIns: null
+//     };
+//   },
+//   mounted() {
+//     const _self = this;
+//     this.$nextTick(function() {
+//       _self.initLocoScroll();
+//     });
+//   },
+//   methods: {
+//     initLocoScroll() {
+//       const _self = this;
+//       this.scroll = new locomotiveScroll({
+//         el: _self.$refs['scrollSections'],
+//         smooth: true,
+//         smoothMobile: true,
+//         getDirection: true
+//       });
+//     }
+//   }
+// };
